427fe373fccea7b93584b1ea6c6ed94020aa987c
bndnt <67003594+bndnt@users.noreply.github.com>
2024-02-12 22:02:39 -0500

    tablet

--- js.js
+++ js.js
@@ -1,1 +1,14 @@
 // const shadowRoot = element.attachShadow({ mode: 'open' });
+// js-close-subscriprion-popup
+const openSubscriptionPopup = document.querySelector('.js-open-subscription-popup');
+const closeSubscriptionPopup = document.querySelector('.js-close-subscription-popup');
+const subscriptionPopup = document.querySelector('.js-subscription-popup');
+
+openSubscriptionPopup.addEventListener('click', function(e){
+    e.preventDefault();
+    subscriptionPopup.classList.add('active');
+})
+closeSubscriptionPopup.addEventListener('click', function(e){
+    e.preventDefault();
+    subscriptionPopup.classList.remove('active');
+})

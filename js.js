cd3aafd02ff9fe971d6b221f1c14f7cfd486f978
bndnt <67003594+bndnt@users.noreply.github.com>
2024-02-12 22:12:19 -0500

    popup

--- js.js
+++ js.js
@@ -1,14 +1,21 @@
-// const shadowRoot = element.attachShadow({ mode: 'open' });
-// js-close-subscriprion-popup
 const openSubscriptionPopup = document.querySelector('.js-open-subscription-popup');
 const closeSubscriptionPopup = document.querySelector('.js-close-subscription-popup');
 const subscriptionPopup = document.querySelector('.js-subscription-popup');
+const bg = document.querySelector('.js-back-drop');
+const body = document.getElementsByName('body');
+
+
 
 openSubscriptionPopup.addEventListener('click', function(e){
     e.preventDefault();
+    bg.classList.add('active');
     subscriptionPopup.classList.add('active');
-})
+    document.body.classList.toggle("no-csroll");
+}) 
+
 closeSubscriptionPopup.addEventListener('click', function(e){
     e.preventDefault();
     subscriptionPopup.classList.remove('active');
+    bg.classList.remove('active');
+    document.body.classList.toggle("no-csroll");
 })

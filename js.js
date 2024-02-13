444bb410bc960749773487cb723ca8cf2d3bdb35
bndnt <67003594+bndnt@users.noreply.github.com>
2024-02-12 22:22:16 -0500

    popup

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 const openSubscriptionPopup = document.querySelector('.js-open-subscription-popup');
 const closeSubscriptionPopup = document.querySelector('.js-close-subscription-popup');
 const subscriptionPopup = document.querySelector('.js-subscription-popup');
+const buttonSubscriptionPopup = document.querySelector('.js-popup__form-btn');
 const bg = document.querySelector('.js-back-drop');
 const body = document.getElementsByName('body');
 
@@ -8,14 +9,21 @@ const body = document.getElementsByName('body');
 
 openSubscriptionPopup.addEventListener('click', function(e){
     e.preventDefault();
-    bg.classList.add('active');
-    subscriptionPopup.classList.add('active');
+    bg.classList.add('is-open');
+    subscriptionPopup.classList.add('is-open');
     document.body.classList.toggle("no-csroll");
 }) 
 
 closeSubscriptionPopup.addEventListener('click', function(e){
     e.preventDefault();
-    subscriptionPopup.classList.remove('active');
-    bg.classList.remove('active');
+    subscriptionPopup.classList.remove('is-open');
+    bg.classList.remove('is-open');
     document.body.classList.toggle("no-csroll");
+})
+
+bg.addEventListener('click', function(e){
+    e.preventDefault();
+    subscriptionPopup.classList.remove('is-open');
+    bg.classList.remove('is-open');
+    document.body.classList.remove("no-csroll");
 })
